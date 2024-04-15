f12b0054c50da7717f554dee20e84077348fc379
hichoudhary22 <hichoudhary22@gmail.com>
2024-04-15 10:05:42 +0530

    sameSite=strict

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -38,7 +38,7 @@ exports.login = async (req, res, next) => {
   const token = secretToken(user._id);
   res.cookie("token", token, {
     withCredentials: true,
-    sameSite: "none",
+    sameSite: "strict",
     // httpOnly: true,
   });
   res.status(200).json({ message: `login succesful ${user.name}` });
