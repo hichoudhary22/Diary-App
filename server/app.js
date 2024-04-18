16e6086f0d8589afadccc7de065e1e526fee384f
hichoudhary22 <hichoudhary22@gmail.com>
2024-04-18 22:37:12 +0530

    temp fix for reload resulting in error 404

--- server/app.js
+++ server/app.js
@@ -22,8 +22,9 @@ app.use(cookieParser());
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/diary", diaryRoute);
-app.use("*", (req, res, next) => {
-  res.status(400).json({ message: "route doesn't exist" });
+app.get("*", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+  // res.status(400).json({ message: "route doesn't exist" });
 });
 
 app.use((err, req, res, next) => {
